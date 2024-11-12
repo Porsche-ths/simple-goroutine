460092f1737ac43fa0d093620bc0bcb52053545e
Thanach Silapatcharanun <porsche.ths@gmail.com>
2024-11-12 14:07:10 +0700

    refactor: data file generator

--- fileGenerator.py
+++ fileGenerator.py
@@ -1,6 +1,9 @@
-file = open("./files/file.csv", "w")
+import random
+import math
+
+file = open("./files/data.csv", "w")
 
 for i in range(1000):
-    file.write(str(i) + "," + str(i) + "\n")
+    file.write("{:.2f}".format(random.random() * 100) + "\n")
 
 file.close()
