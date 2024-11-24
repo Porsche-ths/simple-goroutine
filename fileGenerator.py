3ba98d048bda5088e293f477c3be44f8c30208bf
Thanach Silapatcharanun <porsche.ths@gmail.com>
2024-11-24 21:02:12 +0700

    fix: change to 50M rows of data

--- fileGenerator.py
+++ fileGenerator.py
@@ -2,7 +2,7 @@ import random
 
 file = open("./files/data.csv", "w")
 
-for i in range(10000000):
+for i in range(50000000):
     file.write("{:.2f}".format(random.random() * 100) + "\n")
 
 file.close()
